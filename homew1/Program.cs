1fc5d1d3471ffb3878db8d3cfc6ac8dfdff53f57
Narek <mirzakhanyan99@list.ru>
2022-06-18 17:54:49 +0300

    Сделал красивее

--- homew1/Program.cs
+++ homew1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int n1 , n2;
+
+Console.WriteLine("Введите первое число ");
+n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число ");
+n2 =Convert.ToInt32(Console.ReadLine());
+
+
+
+if (n1 > n2)
+{
+    Console.WriteLine("max " + n1 );
+    Console.WriteLine("min " + n2);
+}
+else
+{
+    Console.WriteLine("max " + n2 );
+    Console.WriteLine("min " + n1);
+}
