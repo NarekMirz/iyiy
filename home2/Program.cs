6dc822a74e2227c7c914f8366c529ed6565985c2
Narek <mirzakhanyan99@list.ru>
2022-06-18 20:36:07 +0300

    Второе задание

--- home2/Program.cs
+++ home2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int a, b, c;
+int max;
+Console.WriteLine("Введите три числа ");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+c = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+
+
+Console.WriteLine("Ваше максимальное " +max);
