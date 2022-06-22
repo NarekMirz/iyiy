e8f941d06a48dfe712d5ec903e0bab5da2bc25d5
Narek <mirzakhanyan99@list.ru>
2022-06-22 23:50:36 +0300

    Четвёртое рабочее задание

--- home4/Program.cs
+++ home4/Program.cs
@@ -1,6 +1,16 @@
-﻿int N;
-Console.WriteLine("Введите длинну диапазона");
-Convert.ToInt32(Console.ReadLine());
+﻿int N, i;
 
-for ( i = 0 ; i < N ;  i ++ )
-       if (i % 2 == 0)
+Console.Write("Выберите диапазон ");
+N = Convert.ToInt32(Console.ReadLine());
+
+i = 1;
+
+while (i <= N)
+{
+    if (i%2 == 0)
+    {   
+      
+       Console.Write(i + " ");
+    }
+    i++;
+}
